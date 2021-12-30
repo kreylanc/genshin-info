7417eec74da124ce1dd77d7b99f8ed26098d431f
kreylanc <devjanpun@gmail.com>
2021-12-30 10:15:05 +0545

    Add artifact page

--- src/NavOptions.js
+++ src/NavOptions.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 function NavOptions({ link, title }) {
   //   const { match, location, history } = withRouter();
   return (
-    <div className="  p-4">
+    <div className=" p-4">
       <NavLink
         to={link}
         className={({ isActive }) =>
@@ -12,7 +12,6 @@ function NavOptions({ link, title }) {
             ? "text-red-500 hover:text-red-500"
             : `h-14 flex items-center p-5 sm:p-0 sm:h-auto text-gray-300 hover:text-gray-100`
         }
-        activeClassName="text-red-500 hover:text-red-500"
       >
         <h2>{title}</h2>
       </NavLink>
---
+++ src/Screens/Artifacts/ArtifactScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ArtifactScreen() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
+
+export default ArtifactScreen;
